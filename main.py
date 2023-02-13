fd881f69692577179f7547634df4505a52479c8b
Soral <cx.soral@gmail.com>
2023-02-13 20:22:48 +0100

    release: Github Action Test

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 app.config.from_object("config." + os.environ.get("XIA_ENV", "prod").title() + "Config")
 
 
+@app.route("/")
+def home():
+    return "Hi"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))  # pragma: no cover
